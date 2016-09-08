efd1ccfa13f3a7f781581658f4e17bcc0cf52a2e
Wei Chen <wei.chen@d2l.com>
2016-09-08 10:57:50 -0400

    Update 2016-09-08T14:57:48.953Z

--- chromecastit.js
+++ chromecastit.js
@@ -1,1 +1,323 @@
-!function(){function e(e){console.log(e)}function t(e){console.log(e)}function n(){var e=new chrome.cast.ApiConfig(new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID),a.bind(null,function(){}),function(e){if("available"===e){console.log("receiver found");var t=JSON.parse(localStorage.getItem("storedSession"));if(t){var n=t.timestamp,i=(new Date).getTime();i-n<y&&t&&(console.log("Found stored session id: "+t.id),chrome.cast.requestSessionById(t.id))}}else console.log("receiver not available")},chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED);chrome.cast.initialize(e,function(){console.log("init success")},t)}function i(e){console.log("launching app"),chrome.cast.requestSession(a.bind(null,e),t)}function a(e,t){if(console.log("Got session: "+t.sessionId),v=t,"undefined"!=typeof Storage){var n={id:t.sessionId,timestamp:(new Date).getTime()};localStorage.setItem("storedSession",JSON.stringify(n))}0!=t.media.length&&l("onRequestSession",t.media[0]),t.addMediaListener(l.bind(this,"addMediaListener")),t.addUpdateListener(o.bind(this)),e()}function o(){v.status==chrome.cast.SessionStatus.STOPPED&&(v=null)}function s(n){v&&(n>0?v.setReceiverVolumeLevel(n,e.bind(this,"set-volume done"),t):v.setReceiverMuted(!0,e.bind(this,"mute done"),t))}function d(e,n,i){if(!v)return void console.log("no session");var a=new chrome.cast.media.MediaInfo(n);a.metadata=new chrome.cast.media.GenericMediaMetadata,a.metadata.metadataType=chrome.cast.media.MetadataType.GENERIC,a.contentType="video/mp4",a.metadata.title=e,a.metadata.images=[{url:i}];var o=new chrome.cast.media.LoadRequest(a);o.autoplay=!0,o.currentTime=0,v.loadMedia(o,l.bind(this,"loadMedia"),t)}function l(e,t){console.log("new media session ID:"+t.mediaSessionId+" via ("+e+")"),g=t,t.addUpdateListener(c)}function c(e){e||(g=null)}function r(){g&&g.play(null,e.bind(this,"playing started for media "+g.sessionId),t)}function u(){g&&g.pause(null,e.bind("paused media "+g.sessionId),t)}function m(n){console.log("Seeking "+g.sessionId+":"+g.mediaSessionId+" to "+n+"%");var i=new chrome.cast.media.SeekRequest;i.currentTime=n*g.media.duration/100,g.seek(i,e.bind(this,"media seek done"),t)}function p(e){if(location.hostname.indexOf("bilibili.com")!=-1){var t=window.aid,n=document.title,i=new XMLHttpRequest;i.onreadystatechange=function(){if(4==this.readyState&&200==this.status){var t=JSON.parse(this.responseText);e(n,t.src,t.img)}},i.open("GET","http://www.bilibili.com/m/html5?aid="+t+"&page=1",!0),i.send()}}function f(){var e=document.createElement("div");e.style.display="block",e.style.position="fixed",e.style.bottom="0px",e.style.left="0px",e.style.height="50vh",e.style.width="100vw",e.style["background-color"]="skyblue",e.style["z-index"]="99999";var t=document.createElement("button");t.innerHTML="Play",t.addEventListener("click",function(){v?p(function(e,t,n){d(e,t,n),r()}):i(function(){p(function(e,t,n){d(e,t,n),r()})})});var n=document.createElement("button");n.innerHTML="Pause",n.addEventListener("click",function(){u()}),n.style.display="none";var a=document.createElement("button");a.innerHTML="Resume",a.addEventListener("click",function(){r()}),a.style.display="none";var o=document.createElement("input");o.type="range",o.min="0",o.max="100",o.step="1",o.style.display="block",o.addEventListener("mouseup",function(){s(this.value/100)});var l=document.createElement("input");l.type="range",l.min="1",l.max="100",l.step="1",l.value="1",l.style.display="block",l.addEventListener("mouseup",function(){m(this.value)}),e.appendChild(t),e.appendChild(n),e.appendChild(a),e.appendChild(l),e.appendChild(o),document.body.appendChild(e),setInterval(function(){v&&g&&g.playerState==chrome.cast.media.PlayerState.PLAYING?(l.value=g.getEstimatedTime()/g.media.duration*100,a.style.display="none",n.style.display="inline-block"):v&&g&&g.playerState==chrome.cast.media.PlayerState.PAUSED&&(a.style.display="inline-block",n.style.display="none")},100)}function h(){var e=document.createElement("script");e.src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js",document.head.appendChild(e)}if(!window.chromecastit){var y=3e5,v=null,g=null;window.__onGCastApiAvailable=function(e,t){e?n():console.log(t)},window.chromecastit=function(e){"main"==e&&(f(),h())}}}();
+(function() {
+  if (window.chromecastit) {
+    return;
+  }
+
+  var SESSION_IDLE_TIMEOUT = 300000;
+
+  var currentSession = null;
+  var currentMedia = null;
+
+  function onSuccess(message) {
+    console.log(message);
+  }
+
+  function onError(e) {
+    console.log(e);
+  }
+
+  function noOp() {
+  }
+
+  window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
+    if (loaded) {
+      initializeCastApi();
+    } else {
+      console.log(errorInfo);
+    }
+  };
+
+  /**
+   * initialization
+   */
+  function initializeCastApi() {
+    var apiConfig = new chrome.cast.ApiConfig(
+      new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID),
+      onSessionConnected.bind(null, noOp),
+      function receiverListener(receiverAvailability) {
+        if (receiverAvailability === 'available') {
+          console.log('receiver found');
+
+          // check if a session ID is saved into localStorage
+          var storedSession = JSON.parse(localStorage.getItem('storedSession'));
+          if (storedSession) {
+            var dateString = storedSession.timestamp;
+            var now = new Date().getTime();
+
+            if (now - dateString < SESSION_IDLE_TIMEOUT) {
+              if (storedSession) {
+                console.log('Found stored session id: ' + storedSession.id);
+                chrome.cast.requestSessionById(storedSession.id);
+              }
+            }
+          }
+        }
+        else {
+          console.log('receiver not available');
+        }
+      },
+      chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED);
+
+    chrome.cast.initialize(
+      apiConfig,
+      function onInitSuccess() {
+        console.log('init success');
+      },
+      onError
+    );
+  }
+
+  function createSession(cb) {
+    console.log('launching app');
+    chrome.cast.requestSession(onSessionConnected.bind(null, cb), onError);
+  }
+
+  function stopSession() {
+    currentSession.stop(function() {
+      console.log('Session stopped');
+    }, onError);
+  }
+
+  function onSessionConnected(cb, session) {
+    console.log('Got session: ' + session.sessionId);
+    currentSession = session;
+
+    //save session id into localstorage
+    if (typeof(Storage) != 'undefined') {
+      // Store sessionId and timestamp into an object
+      var object = {id: session.sessionId, timestamp: new Date().getTime()};
+      localStorage.setItem('storedSession', JSON.stringify(object));
+    }
+
+    if (session.media.length != 0) {
+      onMediaDiscovered('onRequestSession', noOp, session.media[0]);
+    }
+    session.addMediaListener(onMediaDiscovered.bind(this, 'addMediaListener', noOp));
+    session.addUpdateListener(onSessionUpdated.bind(this));
+    cb();
+  }
+
+  function onSessionUpdated() {
+    if (currentSession.status == chrome.cast.SessionStatus.STOPPED) {
+      currentSession = null;
+    }
+  }
+
+  function setVolume(level) {
+    if (!currentSession)
+      return;
+
+    if (level > 0) {
+      currentSession.setReceiverVolumeLevel(
+        level,
+        onSuccess.bind(this, 'set-volume done'),
+        onError);
+    } else {
+      currentSession.setReceiverMuted(
+        true,
+        onSuccess.bind(this, 'mute done'),
+        onError);
+    }
+  }
+
+
+  function loadMedia(mediaTitle, mediaUrl, mediaImageUrl, cb) {
+    if (!currentSession) {
+      console.log('no session');
+      return;
+    }
+
+    var mediaInfo = new chrome.cast.media.MediaInfo(mediaUrl);
+    mediaInfo.metadata = new chrome.cast.media.GenericMediaMetadata();
+    mediaInfo.metadata.metadataType = chrome.cast.media.MetadataType.GENERIC;
+    mediaInfo.contentType = 'video/mp4';
+
+    mediaInfo.metadata.title = mediaTitle;
+    mediaInfo.metadata.images = [{'url': mediaImageUrl}];
+
+    var request = new chrome.cast.media.LoadRequest(mediaInfo);
+    request.autoplay = true;
+    request.currentTime = 0;
+
+    currentSession.loadMedia(request,
+      onMediaDiscovered.bind(this, 'loadMedia', cb),
+      onError);
+  }
+
+  function onMediaDiscovered(how, cb, media) {
+    console.log('new media session ID:' + media.mediaSessionId + ' via (' + how + ')');
+    currentMedia = media;
+    media.addUpdateListener(onMediaUpdated);
+    cb();
+  }
+
+  function getMediaStatus() {
+    if (!currentSession || !currentMedia) {
+      return;
+    }
+
+    currentMedia.getStatus(null,
+      onSuccess.bind(this, 'got media status')
+      , onError);
+  }
+
+  function onMediaUpdated(isAlive) {
+    if (!isAlive) {
+      currentMedia = null;
+    }
+  }
+
+  function playMedia() {
+    if (!currentMedia) {
+      return;
+    }
+    currentMedia.play(
+      null,
+      onSuccess.bind(this, 'playing started for media ' + currentMedia.sessionId),
+      onError);
+  }
+
+  function pauseMedia() {
+    if (!currentMedia) {
+      return;
+    }
+    currentMedia.pause(
+      null,
+      onSuccess.bind('paused media ' + currentMedia.sessionId),
+      onError);
+  }
+
+  function seekMedia(pos) {
+    console.log('Seeking ' + currentMedia.sessionId + ':' +
+      currentMedia.mediaSessionId + ' to ' + pos + '%');
+    var request = new chrome.cast.media.SeekRequest();
+    request.currentTime = pos * currentMedia.media.duration / 100;
+    currentMedia.seek(request,
+      onSuccess.bind(this, 'media seek done'),
+      onError);
+  }
+
+  function findUrl(cb) {
+    if (location.hostname.indexOf('bilibili.com') != -1) {
+      var aid = window.aid;
+      var title = document.title;
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var resp = JSON.parse(this.responseText);
+          cb(title, resp.src, resp.img);
+        }
+      };
+      xhttp.open('GET', 'http://www.bilibili.com/m/html5?aid=' + aid + '&page=1', true);
+      xhttp.send();
+    }
+  }
+
+  function loadUI() {
+    // controls
+    var container = document.createElement('div');
+    container.style.display = 'block';
+    container.style.position = 'fixed';
+    container.style.bottom = '0px';
+    container.style.left = '0px';
+    container.style.height = '50vh';
+    container.style.width = '100vw';
+    container.style['background-color'] = 'skyblue';
+    container.style['z-index'] = '99999';
+
+    var btnPlay = document.createElement('button');
+    btnPlay.innerHTML = 'Play';
+    btnPlay.addEventListener('click', function playListener() {
+      if (!currentSession) {
+        createSession(function() {
+          findUrl(function(title, url, imgUrl) {
+            loadMedia(title, url, imgUrl, function() {
+              playMedia();
+            });
+          });
+        });
+      } else {
+        findUrl(function(title, url, imgUrl) {
+          loadMedia(title, url, imgUrl, function() {
+            playMedia();
+          });
+        });
+      }
+    });
+
+    var btnPause = document.createElement('button');
+    btnPause.innerHTML = 'Pause';
+    btnPause.addEventListener('click', function playListener() {
+      pauseMedia();
+    });
+    btnPause.style.display = 'none';
+
+    var btnResume = document.createElement('button');
+    btnResume.innerHTML = 'Resume';
+    btnResume.addEventListener('click', function playListener() {
+      playMedia();
+    });
+    btnResume.style.display = 'none';
+
+    var inputVolume = document.createElement('input');
+    inputVolume.type = 'range';
+    inputVolume.min = '0';
+    inputVolume.max = '100';
+    inputVolume.step = '1';
+
+    inputVolume.style.display = 'block';
+
+    inputVolume.addEventListener('mouseup', function() {
+      setVolume(this.value / 100);
+    });
+
+    var inputProgress = document.createElement('input');
+    inputProgress.type = 'range';
+    inputProgress.min = '1';
+    inputProgress.max = '100';
+    inputProgress.step = '1';
+    inputProgress.value = '1';
+
+    inputProgress.style.display = 'block';
+
+    inputProgress.addEventListener('mouseup', function() {
+      seekMedia(this.value);
+    });
+
+    container.appendChild(btnPlay);
+    container.appendChild(btnPause);
+    container.appendChild(btnResume);
+    container.appendChild(inputProgress);
+    container.appendChild(inputVolume);
+
+    document.body.appendChild(container);
+
+    setInterval(function uiThread() {
+      if (currentSession &&
+        currentMedia &&
+        currentMedia.playerState == chrome.cast.media.PlayerState.PLAYING) {
+        inputProgress.value = currentMedia.getEstimatedTime() / currentMedia.media.duration * 100;
+        btnResume.style.display = 'none';
+        btnPause.style.display = 'inline-block';
+      } else if (currentSession &&
+        currentMedia &&
+        currentMedia.playerState == chrome.cast.media.PlayerState.PAUSED) {
+        btnResume.style.display = 'inline-block';
+        btnPause.style.display = 'none';
+      }
+    }, 100);
+  }
+
+  function loadSdk() {
+    var chromeCastSdk = document.createElement('script');
+    chromeCastSdk.src = 'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js';
+    document.head.appendChild(chromeCastSdk);
+  }
+
+  window.chromecastit = function main(environment) {
+    if (environment == 'main') {
+      loadUI();
+      loadSdk();
+    }
+  };
+})();
